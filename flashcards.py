317021b1dc55390f99f9478e4adac6d3ff6fd978
Mike Gauthier <gauthiermichael3.14@gmail.com>
2017-03-13 21:03:29 -0500

    flashcards working better

--- flashcards.py
+++ flashcards.py
@@ -1,13 +1,22 @@
+#!/usr/bin/python
 import random, time
 d = dict()
+n = 0
 for line in open('words.txt', 'r'):
-  var = line.split('\t')
-  d[var[len(var)-1]] = var[0]
+  if n < 200: 
+#    var = line.split('\t')
+#    d[var[len(var)-1]] = var[0]
+    n += 1	
+  elif n in range(200,300):
+    var = line.split('\t')
+    d[var[len(var)-1]] = var[0]
+    n += 1
+  else: break
 print 'Number of words: ' + str(len(d))
-print 'Approx time: ' + str(len(d)*2/60) + 'min'
-for y in range(len(d)):
-  x = random.choice(d.keys())
-  print x
+y = 1
+for x in d:
+  print x + '   ' + str(y)
+  y += 1
   raw_input()
   print d[x]
   print '************************************'
