722aa1f455528339193a32ffbdfce0a31fd022ec
Mike Gauthier <gauthiermichael3.14@gmail.com>
2017-03-14 23:16:09 -0500

    update to flashcards

--- flashcards.py
+++ flashcards.py
@@ -3,15 +3,15 @@ import random, time
 d = dict()
 n = 0
 for line in open('words.txt', 'r'):
-  if n <1100: 
+  if n <300: 
 #    var = line.split('\t')
 #    d[var[len(var)-1]] = var[0]
     n += 1	
-  else:
+  elif n in range(300,400):
     var = line.split('\t')
     d[var[len(var)-1]] = var[0]
     n += 1
-#  else: break
+  else: break
 print 'Number of words: ' + str(len(d))
 y = 1
 for x in d:
