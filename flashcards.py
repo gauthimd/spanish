c52b64b3488f644d29c1a36a19d4eddab20c2ca0
Mike Gauthier <gauthiermichael3.14@gmail.com>
2017-04-17 15:22:47 -0500

    more words n such

--- flashcards.py
+++ flashcards.py
@@ -3,20 +3,21 @@ import random, time
 d = dict()
 n = 1
 for line in open('words.txt', 'r'):
-  if n < 600: 
-#    var = line.split('\t')
-#    d[var[len(var)-1]] = var[0]
-    n += 1	
-  elif n in range(600,800):
+  if n < 1200: 
     var = line.split('\t')
     d[var[len(var)-1]] = var[0]
-    n += 1
+    n += 1	
+#  elif n in range(600,800):
+#    var = line.split('\t')
+#    d[var[len(var)-1]] = var[0]
+#    n += 1
   else: break
 print 'Number of words: ' + str(len(d))
 y = 1
 for x in d:
   print x + '   ' + str(y)
   y += 1
-  raw_input()
+#  raw_input()
+  time.sleep(1.5)
   print d[x]
   print '************************************'
